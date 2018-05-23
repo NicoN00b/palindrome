a63a355cd85c9f976db30d9c03b0a4d45c22d8f5
nr <nicholas.raethke@gmail.com>
2018-05-22 23:23:22 -0700

    add business logic

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
-
-
-var input = palInput(str.split([])
+function palindrome(str) {
+  var strReverse;
+  var strOG = str.toLowerCase().replace(/\W|_+/g, '');
+  strReverse = strOG.split('').reverse().join('');
+  if(strReverse === str) {
+    return true;
+  } else {
+    return false;
+  }
+}
